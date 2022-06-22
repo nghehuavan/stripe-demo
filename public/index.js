486f7d6dfcd83d117aafa381bc2e29f2f289f01b
nghe.huavan <nghe.huavan@gmail.com>
2022-06-22 16:55:21 +0700

    .

--- public/index.js
+++ public/index.js
@@ -1,45 +1,41 @@
 // This is your test publishable API key.
-const stripe = Stripe(
-  "pk_test_51HNUusFM58psI0fTpmVDxgBhqmPDyq83iURVuzx4841ZuIrfwoeHQZYQLF8rjKSkQtp9pfWqeJnv0VWWC2M79D1l00N3IFbij1"
-);
+const stripe = Stripe('pk_test_51HNUusFM58psI0fTpmVDxgBhqmPDyq83iURVuzx4841ZuIrfwoeHQZYQLF8rjKSkQtp9pfWqeJnv0VWWC2M79D1l00N3IFbij1');
 
 const reqBody = {
-  order_id: 1111,
-  currency: "usd",
+  order_id: 6789,
+  currency: 'usd',
   amount: 99,
-  description: "description for order: 1111"
+  description: 'nghe.huavan@gmail.com pay for order: 6789',
 };
+document.getElementById('payment-message').innerHTML = '<pre>' + JSON.stringify(reqBody, null, 4) + '</pre>';
 
-document.getElementById("payment-message").innerHTML = JSON.stringify(
-  reqBody,
-  null,
-  4
-);
+checkout=()=>{
+  initialize();
 
-let elements;
-
-initialize();
-
-document
-  .querySelector("#payment-form")
-  .addEventListener("submit", handleSubmit);
+  document.querySelector('#payment-form').addEventListener('submit', handleSubmit);
+}
 
 // Fetches a payment intent and captures the client secret
+let elements;
 async function initialize() {
-  const response = await fetch("/create-payment-intent", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(reqBody)
+  setLoading(true);
+  const response = await fetch('/create-payment-intent', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(reqBody),
   });
   const { id, clientSecret } = await response.json();
-  console.log("response ", id, clientSecret);
+  console.log('response ', id, clientSecret);
   const appearance = {
-    theme: "stripe"
+    theme: 'stripe',
   };
   elements = stripe.elements({ appearance, clientSecret });
 
-  const paymentElement = elements.create("payment");
-  paymentElement.mount("#payment-element");
+  const paymentElement = elements.create('payment');
+  paymentElement.mount('#payment-element');
+  setLoading(false);
+  document.querySelector('#submit').classList.remove('hidden');
+  document.querySelector('#btn-checkout').classList.add('hidden');
 }
 
 async function handleSubmit(e) {
@@ -50,9 +46,8 @@ async function handleSubmit(e) {
     elements,
     confirmParams: {
       // Make sure to change this to your payment completion page
-      return_url:
-        window.location.origin + window.location.pathname + "result.html"
-    }
+      return_url: window.location.origin + window.location.pathname + 'result.html',
+    },
   });
 
   // This point will only be reached if there is an immediate error when
@@ -60,10 +55,10 @@ async function handleSubmit(e) {
   // your `return_url`. For some payment methods like iDEAL, your customer will
   // be redirected to an intermediate site first to authorize the payment, then
   // redirected to the `return_url`.
-  if (error.type === "card_error" || error.type === "validation_error") {
+  if (error.type === 'card_error' || error.type === 'validation_error') {
     showMessage(error.message);
   } else {
-    showMessage("An unexpected error occurred.");
+    showMessage('An unexpected error occurred.');
   }
 
   setLoading(false);
@@ -71,14 +66,14 @@ async function handleSubmit(e) {
 
 // ------- UI helpers -------
 function showMessage(messageText) {
-  const messageContainer = document.querySelector("#payment-message");
+  const messageContainer = document.querySelector('#payment-message');
 
-  messageContainer.classList.remove("hidden");
+  messageContainer.classList.remove('hidden');
   messageContainer.textContent = messageText;
 
   setTimeout(function () {
-    messageContainer.classList.add("hidden");
-    messageText.textContent = "";
+    messageContainer.classList.add('hidden');
+    messageText.textContent = '';
   }, 4000);
 }
 
@@ -86,12 +81,14 @@ function showMessage(messageText) {
 function setLoading(isLoading) {
   if (isLoading) {
     // Disable the button and show a spinner
-    document.querySelector("#submit").disabled = true;
-    document.querySelector("#spinner").classList.remove("hidden");
-    document.querySelector("#button-text").classList.add("hidden");
+    document.querySelector('#submit').disabled = true;
+    document.querySelector('#spinner').classList.remove('hidden');
+    document.querySelector('#button-text').classList.add('hidden');
+    document.querySelector('#button-text1').classList.add('hidden');
   } else {
-    document.querySelector("#submit").disabled = false;
-    document.querySelector("#spinner").classList.add("hidden");
-    document.querySelector("#button-text").classList.remove("hidden");
+    document.querySelector('#submit').disabled = false;
+    document.querySelector('#spinner').classList.add('hidden');
+    document.querySelector('#button-text').classList.remove('hidden');
+    document.querySelector('#button-text1').classList.remove('hidden');
   }
 }
